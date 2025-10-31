535c6f370b4eafcc809304a0847f4fe2ab1094be
devesh-qurilo <devesh@qurilo.com>
2025-10-31 16:40:47 +0530

    Admin

---
+++ ios/skova/skovaApp.swift
@@ -0,0 +1,17 @@
+//
+//  skovaApp.swift
+//  skova
+//
+//  Created by Hemant Sharma on 10/31/25.
+//
+
+import SwiftUI
+
+@main
+struct skovaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
